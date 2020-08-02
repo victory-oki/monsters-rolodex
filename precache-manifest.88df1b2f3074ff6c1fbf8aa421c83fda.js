ce23080f752d82e2eb2d15229cb4b02ba81b7e70
victory-oki <victorysplogi@gmail.com>
2020-08-02 04:04:06 +0100

    Updates

---
+++ precache-manifest.88df1b2f3074ff6c1fbf8aa421c83fda.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "385fef117e75f5644e3b4aa868619c9f",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "7502bbedb4ad880ecfcc",
+    "url": "/monsters-rolodex/static/css/main.6f54d3ca.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7502bbedb4ad880ecfcc",
+    "url": "/monsters-rolodex/static/js/main.f8189cec.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/monsters-rolodex/static/media/logo.5d5d9eef.svg"
+  }
+]);
